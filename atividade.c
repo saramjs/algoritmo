2bbdeee3f9d8112ba63bf3573ce0bf03595522c5
saramjs <135473303+saramjs@users.noreply.github.com>
2023-09-10 13:31:53 -0300

    Add files via upload

---
+++ atividade.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    int abc, cba, xyz, zyx, resultado;
+    
+    // Solicitar que o usuário digite um número abc de 3 algarismos
+    
+    printf("Digite um número de 3 algarismos (100 <= x <= 999): ");
+    scanf("%d", &abc);
+    
+    // Verificar se o número tem 3 algarismos
+    
+    if (abc < 100 || abc > 999) {
+        printf("O número não possui 3 algarismos.\n");
+    } else {
+    	
+        // Verificar se a diferença entre algarismos das centenas e das unidades é maior ou igual a 2
+        
+        int centenas = abc / 100;
+        int unidades = abc % 10;
+        
+        if (centenas - unidades < 2) {
+            printf("A diferença entre as centenas e unidades não é maior ou igual a 2.\n");
+        } else {
+        	
+            //Inverter a ordem dos algarismos formando o número cba
+            
+            cba = unidades * 100 + (abc / 10) % 10 * 10 + centenas;
+            
+            //Somar abc e cba, obtendo o número xyz
+            
+            xyz = abc + cba;
+            
+            //Inverter a ordem de xyz, obtendo zyx
+            
+            zyx = (xyz % 10) * 100 + ((xyz / 10) % 10) * 10 + (xyz / 100);
+            
+            //Somar xyz e zyx para obter o resultado
+            
+            resultado = xyz + zyx;
+            
+            //Imprimir o resultado
+            
+            printf("Resultado: %d\n", resultado);
+        }
+    }
+    
+    return 0;
+}
+
